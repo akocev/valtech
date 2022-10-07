4e3ccfa00cb5b03e5fa71570c7f8a34b48c95108
Aleksandra Koceva <aleksandra.koceva@swisscom.com>
2022-10-08 00:21:11 +0200

    Created valstock project

---
+++ webpack.dev.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+var webpackConfig = {
+
+entry: {
+  main: "./src/main.ts"
+},
+mode: 'development',
+resolve: {
+  extensions: ['.ts', '.js']
+},
+output: {
+  filename: 'bundle.js',
+  path: path.join(__dirname, 'src', 'dist')
+},
+module: {
+  rules: [
+    {
+      test: /\.ts$/,
+      use: ['ts-loader', 'angular2-template-loader']
+    },
+    {
+      test: /\.(png|jpg)$/,
+      type: 'asset/resource'
+  },
+    {
+      test: /\.html$/,
+      use: 'html-loader'
+    },
+    {
+      test:  /\.css$/,
+      use: [
+          'style-loader', 'css-loader'
+      ]
+  },
+  {
+      test:  /\.scss$/,
+      use: [
+          'style-loader', 'css-loader', 'sass-loader'
+      ]
+  },
+  ]
+},
+}
+webpackConfig.plugins = [
+  new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })
+];
+module.exports = webpackConfig;
